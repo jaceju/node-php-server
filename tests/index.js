9b91d613c67d629029df7e2fffddf2d7bb031a48
Jace Ju <jaceju@gmail.com>
2014-07-12 14:29:36 +0800

    Add server status check.

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,8 @@
-var gulp module = require('../lib');
+var phpServer = require('../lib');
 var demand = require('must');
 
 describe('node-php-server', function () {
   it('should exist', function () {
-    demand(gulp module).to.exist();
+    demand(phpServer).to.exist();
   });
 });
