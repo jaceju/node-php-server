5c1c2653ba823a71a5a52aca662fc312f1529737
Jace Ju <jaceju@gmail.com>
2014-07-14 10:01:07 +0800

    Refactor man script and add tests.

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,23 @@
-var phpServer = require('../lib');
-var demand = require('must');
+var phpServer = require('../lib'),
+    http = require('http'),
+    expect = require('must');
 
-describe('node-php-server', function () {
-  it('should exist', function () {
-    demand(phpServer).to.exist();
-  });
+describe('node-php-server', function() {
+    before(function () {
+        phpServer.createServer({
+            port: 8000,
+            router: __dirname + '/../lib/server.php'
+        });
+        console.log('\nStarted');
+    });
+
+    after(function () {
+        phpServer.close();
+        console.log('\nClosed');
+    });
+
+    it('should exist', function() {
+        expect(phpServer).to.exist();
+    });
+
 });
