4da13cb19278b4f9d423ad0068ec80ef16b97b5a
Jace Ju <jaceju@gmail.com>
2014-07-14 14:25:08 +0800

    Add sleep for testing.

--- tests/index.js
+++ tests/index.js
@@ -1,93 +1,40 @@
 var phpServer = require('../lib'),
     http = require('http'),
+    sleep = require('sleep'),
     expect = require('must');
 
 describe('node-php-server', function() {
+
     before(function () {
         phpServer.createServer({
             port: 8000,
             router: __dirname + '/../examples/server.php'
         });
-        console.log('\nStarted');
+        sleep.sleep(1);
     });
 
     after(function () {
         phpServer.close();
-        console.log('\nClosed');
     });
 
-    it('should exist', function() {
-        expect(phpServer).to.exist();
-    });
+    describe('fetch data', function() {
 
-    // it('should return 200', function(done) {
-    //     console.log('Opening...');
-    //     http.get('http://127.0.0.1:8000/', function(res) {
-    //         console.log('Checking...');
-    //         // expect(res.statusCode).to.equal(200);
-    //         assert.equal(200, res.statusCode);
-    //         done();
-    //     });
-    // });
+        it('should exist', function() {
+            expect(phpServer).to.exist();
+        });
+
+        it('should return 200', function(done) {
+            http.get('http://127.0.0.1:8000/', function(res) {
+                res.on('data', function() {
+                });
+
+                res.on('end', function() {
+                    expect(res.statusCode).to.equal(200);
+                    done();
+                });
+            });
+        });
+
+    });
 });
 
-// var instance;
-
-// describe('Tests', function() {
-
-//     before(function() {
-//         instance = http.createServer(function(request, response) {
-//             console.log(request.url);
-//         }).listen(8000);
-//         instance.on("listening", function() {
-//             console.log("Started");
-//         });
-//     });
-
-//     after(function(done) {
-//         instance.close();
-//         console.log("Stopped");
-//         done();
-//     });
-
-//     it("Should fetch test.html", function(done) {
-//         console.log("Fetching URL...");
-//         http.get({
-//             host: "localhost",
-//             port:8000,
-//             path: "/"
-//         }, function(res) {
-//             console.log('OK');
-//             expect(res.statusCode).toEqual(200);
-//             done();
-//         })
-//     });
-
-// });
-
-// describe('server', function() {
-//     var server;
-
-//     before(function() {
-//         server = http.createServer()
-//             .listen(8000)
-//             .on('listening', function() {
-//                 console.log('Started connect web server on http://localhost:8000');
-//             });
-//     });
-
-//     after(function() {
-//         server.close(function () {
-//             console.log('Closing...');
-//         });
-//     });
-// });
-
-// describe('/', function() {
-//     it('should return 200', function(done) {
-//         http.get('http://localhost:8000', function(res) {
-//             expect(res.statusCode).to.equal(200);
-//             done();
-//         });
-//     });
-// });
