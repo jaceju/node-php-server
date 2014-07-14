49c473c9cfd621bf1bfbaeeba47c86f7e51879f3
Jace Ju <jaceju@gmail.com>
2014-07-14 10:22:22 +0800

    Update README.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # node-php-server [![Build Status](https://travis-ci.org/jaceju/node-php-server.svg?branch=master)](https://travis-ci.org/jaceju/node-php-server)
 
-Create a built-in PHP server for node.js.
+Create a built-in PHP server for PHP framework in node.js script.
 
 ## Install
 
@@ -11,7 +11,7 @@ npm install --save node-php-server
 ## Examples
 
 ```js
-var phpServer require('node-php-server');
+var phpServer = require('node-php-server');
 
 // Create a PHP Server
 phpServer.createServer({
@@ -21,11 +21,30 @@ phpServer.createServer({
     keepalive: false,
     open: false,
     bin: 'php',
-    router: __dirname + '/../lib/server.php'
+    router: __dirname + '/server.php'
 });
 
 // Close server
 phpServer.close();
+```
+
+`server.php`
+
+```php
+<?php
+
+call_user_func(function () {
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $publicDir = __DIR__ . '/public/';
+    $uri = urldecode($uri);
+    $requested = $publicDir . $uri;
+
+    if ($uri !== '/' && file_exists($requested)) {
+        return false;
+    }
+
+    require_once $publicDir . 'index.php';
+});
 ```
 
 ## License
--- tests/index.js
+++ tests/index.js
@@ -20,4 +20,74 @@ describe('node-php-server', function() {
         expect(phpServer).to.exist();
     });
 
+    // it('should return 200', function(done) {
+    //     console.log('Opening...');
+    //     http.get('http://127.0.0.1:8000/', function(res) {
+    //         console.log('Checking...');
+    //         // expect(res.statusCode).to.equal(200);
+    //         assert.equal(200, res.statusCode);
+    //         done();
+    //     });
+    // });
 });
+
+// var instance;
+
+// describe('Tests', function() {
+
+//     before(function() {
+//         instance = http.createServer(function(request, response) {
+//             console.log(request.url);
+//         }).listen(8000);
+//         instance.on("listening", function() {
+//             console.log("Started");
+//         });
+//     });
+
+//     after(function(done) {
+//         instance.close();
+//         console.log("Stopped");
+//         done();
+//     });
+
+//     it("Should fetch test.html", function(done) {
+//         console.log("Fetching URL...");
+//         http.get({
+//             host: "localhost",
+//             port:8000,
+//             path: "/"
+//         }, function(res) {
+//             console.log('OK');
+//             expect(res.statusCode).toEqual(200);
+//             done();
+//         })
+//     });
+
+// });
+
+// describe('server', function() {
+//     var server;
+
+//     before(function() {
+//         server = http.createServer()
+//             .listen(8000)
+//             .on('listening', function() {
+//                 console.log('Started connect web server on http://localhost:8000');
+//             });
+//     });
+
+//     after(function() {
+//         server.close(function () {
+//             console.log('Closing...');
+//         });
+//     });
+// });
+
+// describe('/', function() {
+//     it('should return 200', function(done) {
+//         http.get('http://localhost:8000', function(res) {
+//             expect(res.statusCode).to.equal(200);
+//             done();
+//         });
+//     });
+// });
